64219dbbe32155df50b33949d735fccd6a6a40cd
eyongclinton <150918279+eyongclinton@users.noreply.github.com>
2024-03-18 23:58:04 +0000

    Initial commit

---
+++ blog_api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import PostList, PostDetail
+
+app_name = "blog_api"
+
+urlpatterns = [
+    # /api/posts - list of all
+    path('<int:pk>/', PostDetail.as_view(), name='detailcreate'),
+    path('', PostList.as_view(), name='listcreate'),
+]
---
+++ blog_api/views.py
@@ -0,0 +1,18 @@
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
+
+# Create your views here.
+
+
+class PostList(generics.ListCreateAPIView):
+  queryset = Post.postobjects.all()  # Fix typo here
+  serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveAPIView):
+  queryset = Post.postobjects.all()
+  serializer_class = PostSerializer
+
+
+# Fix typo here
